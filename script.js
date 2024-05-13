a76a6ac4816b3e7eaabc43f0758e9b69068496f7
Alena Kadecká <kadecka.alena@gmail.com>
2024-05-13 23:00:03 +0200

    imperfect playning with images

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function changeBackground() {
+  var images = [
+      'img/padrian1.jpg',
+      'img/padrian2.jpg',
+      'img/coding.jpg',
+      'img/pexels.jpg',
+      'img/flower.jpg',
+      'img/woman.jpg',
+      'img/colors.jpg',
+      'img/notebook.jpg',
+  ];
 
+  var randomImage = images[Math.floor(Math.random() * images.length)];
+  document.body.style.backgroundImage = 'url(' + randomImage + ')';
+}
