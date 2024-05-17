6a213b32442c8f33f28176086fd6fe09d88ae0b2
Alena Kadecká <kadecka.alena@gmail.com>
2024-05-17 20:22:24 +0200

    uprava -doladenie JS

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 function changeBackground() {
   var images = [
-      'img/coding.jpg',
-      'img/pexels.jpg',
-      'img/flower.jpg',
-      'img/woman.jpg',
-      'img/colors.jpg',
-      'img/notebook.jpg',
+    'img/coding.jpg',
+    'img/pexels.jpg',
+    'img/flower.jpg',
+    'img/woman.jpg',
+    'img/colors.jpg',
+    'img/notebook.jpg',
   ];
 
   var randomImage = images[Math.floor(Math.random() * images.length)];
   document.body.style.backgroundImage = 'url(' + randomImage + ')';
 }
+
+changeBackground();
